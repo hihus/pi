25d4c71aa3c11101fb428014382a477468a5e9a2
hihu <hihu@jiayuandeMacBook-Air.local>
2017-11-06 11:28:52 +0800

    test

--- proj/app/web/index.php
+++ proj/app/web/index.php
@@ -4,7 +4,7 @@ define('TIMEZONE','Asia/Shanghai');
 define("__PI_EN_DEBUG",1);
 //必要配置
 define('PI_APP_NAME','web');
-define('PI_ROOT',dirname(dirname(dirname(__FILE__))).'/pi/');
+define('PI_ROOT',dirname(dirname(dirname(dirname(__FILE__)))).'/pi/');
 define('PI_APP_ROOT',dirname(dirname(__FILE__)).'/');
 define('PI_COM_ROOT',PI_APP_ROOT.'com/');
 define('APP_CTR_ROOT',PI_APP_ROOT.PI_APP_NAME.'/ctr/');
